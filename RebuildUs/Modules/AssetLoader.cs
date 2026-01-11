dffc516245d4e301c76dc4c3683d8a60286ced08
Shota-Sunada <shota.sunada.dev@gmail.com>
2026-01-11 17:23:07 +0900

    AssetLoader準備

---
+++ RebuildUs/Modules/AssetLoader.cs
@@ -0,0 +1,47 @@
+using System.Reflection;
+
+namespace RebuildUs.Modules;
+
+public static class AssetLoader
+{
+    private static bool isLoaded = false;
+    public static void LoadAssets()
+    {
+        if (isLoaded) return;
+        isLoaded = true;
+
+        LoadButtonAssets();
+        LoadAnimationAssets();
+    }
+
+    private static void LoadButtonAssets()
+    {
+        var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream("RebuildUs.Resources.AssetBundle.buttons");
+    }
+
+    private static void LoadAnimationAssets()
+    {
+        var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream("RebuildUs.Resources.AssetBundle.animations");
+    }
+
+    public static byte[] ReadFully(this Stream input)
+    {
+        using var ms = new MemoryStream();
+        input.CopyTo(ms);
+        return ms.ToArray();
+    }
+
+#nullable enable
+    private static T? LoadAsset<T>(this AssetBundle assetBundle, string name) where T : UnityEngine.Object
+    {
+        return assetBundle.LoadAsset(name, Il2CppType.Of<T>())?.Cast<T>();
+    }
+#nullable disable
+
+    private static T DontUnload<T>(this T obj) where T : UnityEngine.Object
+    {
+        obj.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+
+        return obj;
+    }
+}
