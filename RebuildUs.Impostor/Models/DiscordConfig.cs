ed01d18971870c9f06a88cc0acd7c00eed98146d
Shota-Sunada <shota.sunada.dev@gmail.com>
2026-02-21 14:51:59 +0900

    Revert "サーバープラグイン削除"
    This reverts commit 2c826db03f54a29be34a9312810c0769b6f2319a.

---
+++ RebuildUs.Impostor/Models/DiscordConfig.cs
@@ -0,0 +1,12 @@
+namespace RebuildUs.Impostor.Models;
+
+public class DiscordConfig
+{
+    public bool DisableDiscord { get; set; }
+    public bool AutoMute { get; set; } = true;
+    public string Token { get; set; } = string.Empty;
+    public List<string> WorkerTokens { get; set; } = [];
+    public ulong GuildId { get; set; }
+    public ulong VoiceChannelId { get; set; }
+    public ulong TextChannelId { get; set; }
+}
