3cf26c1e01e42112e700f86f61fea669cd76ac8f
Shota-Sunada <shota.sunada.dev@gmail.com>
2026-03-20 18:25:37 +0900

    Revert "削除"
    This reverts commit ad4da96ec3b80314e3c00ce954cd684d2558b19f.

---
+++ RebuildUs/Roles/Crewmate/Sucider.cs
@@ -0,0 +1,158 @@
+namespace RebuildUs.Roles.Crewmate;
+
+[HarmonyPatch]
+[RegisterRole(RoleType.Suicider, RoleTeam.Crewmate, typeof(MultiRoleBase<Suicider>), nameof(Suicider.NameColor), nameof(CustomOptionHolder.SuiciderSpawnRate))]
+internal class Suicider : MultiRoleBase<Suicider>
+{
+    internal static Color NameColor = Palette.ImpostorRed;
+
+    private static CustomButton _suicideButton;
+
+    public Suicider()
+    {
+        // write value init here
+        StaticRoleType = CurrentRoleType = RoleType.Suicider;
+    }
+
+    internal override Color RoleColor
+    {
+        get => NameColor;
+    }
+
+    internal static bool CanEnterVents
+    {
+        get => CustomOptionHolder.SuiciderCanEnterVents.GetBool();
+    }
+
+    internal static bool HasImpostorVision
+    {
+        get => CustomOptionHolder.SuiciderHasImpostorVision.GetBool();
+    }
+
+    internal static bool CanFixComm
+    {
+        get => CustomOptionHolder.SuiciderCanFixComm.GetBool();
+    }
+
+    internal static bool CanKnowImpostorAfterFinishTasks
+    {
+        get => CustomOptionHolder.SuiciderCanKnowImpostorAfterFinishTasks.GetBool();
+    }
+
+    internal static int NumCommonTasks
+    {
+        get => CustomOptionHolder.SuiciderTasks.CommonTasksNum;
+    }
+
+    internal static int NumLongTasks
+    {
+        get => CustomOptionHolder.SuiciderTasks.LongTasksNum;
+    }
+
+    internal static int NumShortTasks
+    {
+        get => CustomOptionHolder.SuiciderTasks.ShortTasksNum;
+    }
+
+    internal override void OnUpdateNameColors()
+    {
+        if (Player == PlayerControl.LocalPlayer)
+        {
+            HudManagerPatch.SetPlayerNameColor(Player, NameColor);
+
+            if (KnowsImpostors(Player))
+            {
+                foreach (var p in PlayerControl.AllPlayerControls.GetFastEnumerator())
+                {
+                    if (p.IsTeamImpostor()
+                        || p.IsRole(RoleType.Spy)
+                        || p.IsRole(RoleType.Jackal) && Jackal.Instance.WasTeamRed
+                        || p.IsRole(RoleType.Sidekick) && Sidekick.Instance.WasTeamRed)
+                    {
+                        HudManagerPatch.SetPlayerNameColor(p, Palette.ImpostorRed);
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    [RegisterCustomButton]
+    internal static void MakeButtons(HudManager hm)
+    {
+        _suicideButton = new(() =>
+            {
+                {
+                    using RPCSender sender = new(PlayerControl.LocalPlayer.NetId, CustomRPC.UncheckedMurderPlayer);
+                    sender.Write(PlayerControl.LocalPlayer.PlayerId); // source
+                    sender.Write(PlayerControl.LocalPlayer.PlayerId); // target
+                    sender.Write((byte)1); // showAnimation
+                }
+                RPCProcedure.UncheckedMurderPlayer(PlayerControl.LocalPlayer.PlayerId, PlayerControl.LocalPlayer.PlayerId, 1);
+            },
+            () =>
+            {
+                return PlayerControl.LocalPlayer.IsRole(RoleType.Suicider) && PlayerControl.LocalPlayer?.Data?.IsDead == false;
+            },
+            () =>
+            {
+                return PlayerControl.LocalPlayer.CanMove;
+            },
+            () =>
+            {
+                _suicideButton.Timer = _suicideButton.MaxTimer;
+            },
+            hm.KillButton.graphic.sprite,
+            ButtonPosition.Layout,
+            hm,
+            hm.KillButton,
+            AbilitySlot.CrewmateAbilityPrimary,
+            false,
+            0f,
+            null,
+            false,
+            Tr.Get(TrKey.Suicide));
+    }
+
+    [RegisterCustomButton]
+    internal static void SetButtonCooldowns()
+    {
+        _suicideButton?.MaxTimer = 0f;
+    }
+
+    internal static bool KnowsImpostors(PlayerControl player)
+    {
+        return CanKnowImpostorAfterFinishTasks && IsRole(player) && TasksComplete(player);
+    }
+
+    internal static bool TasksComplete(PlayerControl player)
+    {
+        if (!CanKnowImpostorAfterFinishTasks)
+        {
+            return false;
+        }
+
+        var counter = 0;
+        var totalTasks = NumCommonTasks + NumLongTasks + NumShortTasks;
+        if (totalTasks == 0)
+        {
+            return true;
+        }
+        foreach (var task in player.Data.Tasks)
+        {
+            if (task.Complete)
+            {
+                counter++;
+            }
+        }
+
+        return counter == totalTasks;
+    }
+
+    internal static void Clear()
+    {
+        // reset configs here
+        Players.Clear();
+    }
+}
